a56e39dc1054c1717e9d99bf1217300eb4f60dc4
AxorXT <angel.sanchez@uniat.edu.mx>
2024-09-17 14:27:23 -0600

    player
    dd

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -1,39 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+public class MoveCamera : MonoBehaviour
 {
-    //Variables publicas
-    public float rotationSpeed, minRotation, maxRotation;
-    public bool canRotate;
 
-    //Variables privadas
-    private float xRotation, yRotation;
+    public Transform player;
 
-   
-
-    // Update is called once per frame
     void Update()
     {
-        Rotation();
-    }
-
-    public void Rotation()
-    {
-        //Si podemos
-        if (canRotate)
-        {
-            //Conseguimos la rotacion del mouse
-            xRotation += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            yRotation += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-
-            //Agregamos el limite de rotacion Y
-            yRotation = Mathf.Clamp(yRotation, minRotation, maxRotation);
-
-            //Rotar el objeto y la camara
-            transform.localRotation = Quaternion.Euler(0f, xRotation, 0f);
-            Camera.main.transform.localRotation = Quaternion.Euler(-yRotation, 0f, 0f);
-        }
+        transform.position = player.transform.position;
     }
 }
